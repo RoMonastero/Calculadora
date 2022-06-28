cf10f450bbc40a516e7152d58620331929660252
RoMonastero <romonastero@gmail.com>
2022-06-28 17:26:21 -0300

    Adicionando funções com JavaCript

---
+++ main/script.js
@@ -0,0 +1,53 @@
+const result = document.getElementById("result")
+
+function clean() {
+    if (result.textContent != "0") {
+        result.innerText = "0"
+    }
+}
+
+function addNumber(num) {
+    if (result.textContent.length <= 19) {
+        if (result.textContent == "0") {
+            result.innerText = num
+        }else{
+            result.innerText += num
+        } 
+    }
+    
+}
+
+function addoperator(operator) {
+    if (result.textContent.length <= 19) {
+        if (result.textContent != "0" && !(result.textContent.endsWith('+') || result.textContent.endsWith('-') || result.textContent.endsWith('*') || result.textContent.endsWith('/'))) {
+            result.innerText += operator
+         } 
+    }
+    
+}
+
+function addDot(){  
+    if (result.textContent.length <= 19) {
+        result.innerText += '.'   
+    }
+   
+}
+
+function calculate() {
+    if (result.textContent.endsWith('+') || result.textContent.endsWith('-') || result.textContent.endsWith('*') || result.textContent.endsWith('/')) {
+        alert("Não é possivel fazer uma conta com um operador como útlimo elemento")
+    }else{
+        result.innerText = eval(result.textContent)
+    }
+    
+}
+
+function backSpace() {
+    if (result.textContent != "0") {
+        if (result.textContent.substring(0, result.textContent.length -1) == '') {
+            result.innerText = '0' 
+        } else {
+            result.innerText = result.textContent.substring(0, result.textContent.length -1)
+        }    
+    }  
+}
